7c46312a324360fe91d901c3ba01cdccec1d5955
Caleb Harris <charris@wpi.edu>
2025-11-30 14:36:27 -0500

    PA 0.0t: Updated the testing data

--- REMain.cpp
+++ REMain.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main() {
 
     vector<string> wdarfs = {"Wilyer", "David", "Abreu", "RF", "L", "L"};
-    vector<int> wdarfnCS = {81, 265, 39, 68, 133, 11, 0, 18, 52, 28, 4, 68, 5, 2};
-    vector<int> wdarfnACS = {295, 29, 0, 0, 2, 6, 38, 1123, 0, 131, 1};
+    vector<int> wdarfnCS = {115, 373, 53, 92, 175, 17, 0, 22, 69, 40, 5, 101, 6, 3};
+    vector<int> wdarfnACS = {417, 39, 0, 1, 3, 8, 53, 1600, 0, 179, 1};
 
     vector<float> wdarfnf = {playerAVG(wdarfnCS), 
                              playerOBP(wdarfnCS), 
@@ -33,6 +33,6 @@ int main() {
      //     G,    AB,     R,     H,    TB,    2B,    3B,    HR,   RBI,    BB,    IBB,     SO,     SB,     CS,  AVG,  OBP,  SLG,  OPS, GO/AO
      // CS[0], CS[1], CS[2], CS[3], CS[4], CS[5], CS[6], CS[7], CS[8], CS[9], CS[10], CS[11], CS[12], CS[13], F[0], F[1], F[2], F[3], F[4]
     // ACS:
-     // PA, XBH, HBP, SAC, SF, BABIP, GIDP, GIDPO, NP, P/PA, ROE, LOB, WO
-     // ACS[0], ACS[1], ACS[2], ACS[3], ACS[4], ACS[5], F[6], ACS[7], ACS[8], ACS[9], ACS[10], ACS[11], ACS[12]
+     //     PA,     XBH,    HBP,    SAC,     SF,  BABIP, GIDP,  GIDPO,     NP,   P/PA,     ROE,     LOB,      WO
+     // ACS[0],  ACS[1], ACS[2], ACS[3], ACS[4], ACS[5], F[6], ACS[7], ACS[8], ACS[9], ACS[10], ACS[11], ACS[12]
 }
